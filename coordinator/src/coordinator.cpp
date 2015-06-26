eb9878e3f1b30485e4c05791b0d9810451369089
Wyatt Newman <wsn@case.edu>
2015-06-26 16:24:30 -0400

    coordinator node progress

--- coordinator/src/coordinator.cpp
+++ coordinator/src/coordinator.cpp
@@ -15,7 +15,10 @@
 #include <stdio.h>
 #include <vector>
 #include <iostream>
-//#include <Eigen/Eigen>  
+#include <Eigen/Eigen>  
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <Eigen/Dense>
 #include <cwru_srv/IM_node_service_message.h>
 #include <cwru_srv/simple_float_service_message.h>
 #include <cwru_srv/simple_int_service_message.h>
@@ -24,63 +27,64 @@
 #include <tf/LinearMath/Vector3.h>
 #include <tf/LinearMath/QuadWord.h>
 
-//#include <cwru_srv/>
-//command codes for interactive marker: get/set pose
-const int GET_CURRENT_MARKER_POSE = 0;
-const int SET_NEW_MARKER_POSE = 1;
-
-
-
 const double H_CYLINDER = 0.24; // estimated height of cylinder
 
 using namespace std;
 
-//incoming command codes to this service:
-const int DO_NOTHING = 0;
-const int DISPLAY_REACHABILITY_AT_MARKER_HEIGHT = 1;
-const int COMPUTE_MOVE_ARM_TO_PRE_POSE = 2;
-const int MOVE_ARM_TO_APPROACH_POSE = 3;
-const int MOVE_ARM_TO_GRASP_POSE = 4;
-const int MOVE_ARM_DEPART = 5;
-const int MOVE_ARM_TO_MARKER = 6;
-const int MOVE_MARKER_TO_GRASP_POSE = 7;
+//#include <cwru_srv/>
+//command codes for interactive marker: get/set pose
+cwru_srv::IM_node_service_message pcl_getframe_msg;
+    cwru_srv::IM_node_service_message IM_6dof_srv_msg;
+const int IM_GET_CURRENT_MARKER_POSE=0;
+const int IM_SET_NEW_MARKER_POSE= 1;
+
+//command codes for reachability node--only 1 action at present, so only need to provide z-height value
+cwru_srv::simple_float_service_message reachability_msg;
+
+
+
+//incoming command codes for this coordinator service:
+const int RQST_DO_NOTHING = 0;
+const int RQST_DISPLAY_REACHABILITY_AT_MARKER_HEIGHT = 1;
+const int RQST_COMPUTE_MOVE_ARM_TO_PRE_POSE = 2;
+const int RQST_MOVE_ARM_TO_APPROACH_POSE = 3;
+const int RQST_MOVE_ARM_TO_GRASP_POSE = 4;
+const int RQST_MOVE_ARM_DEPART = 5;
+const int RQST_MOVE_ARM_TO_MARKER = 6;
+const int RQST_MOVE_MARKER_TO_GRASP_POSE = 7;
 const int RQST_DO_PLANNED_PATH=8;
 const int RQST_DESCEND_20CM=9;
 const int RQST_ASCEND_20CM=10;
 
-//service codes to send to arm interface:
-const int TEST_MODE =0;
-const int GO_TO_PREDFINED_PRE_POSE=1;
-const int DESCEND_20CM=2;
-const int DEPART_20CM=3;
+//service codes to send to arm interface: these are in cartesian_moves/arm_motion_interface_defs.h
+cwru_srv::arm_nav_service_message arm_nav_msg;
+const int ARM_TEST_MODE =0;
+const int ARM_GO_TO_PREDFINED_PRE_POSE=1;
+const int ARM_DESCEND_20CM=2;
+const int ARM_DEPART_20CM=3;
+const int ARM_IS_SERVER_BUSY_QUERY = 1;
+const int ARM_PLAN_PATH_QSTART_TO_ADES = 4;
+const int ARM_PLAN_PATH_QSTART_TO_QGOAL = 5;
+const int ARM_PLAN_PATH_ASTART_TO_QGOAL = 6;
+const int ARM_PLAN_PATH_CURRENT_TO_PRE_POSE=7;
+const int ARM_GET_Q_DATA = 8;
+const int ARM_EXECUTE_PLANNED_PATH = 9;
 
-const int IS_SERVER_BUSY_QUERY = 1;
-const int PLAN_PATH_QSTART_TO_ADES = 4;
-const int PLAN_PATH_QSTART_TO_QGOAL = 5;
-const int PLAN_PATH_ASTART_TO_QGOAL = 6;
-const int PLAN_PATH_CURRENT_TO_PRE_POSE=7;
-
-const int GET_Q_DATA = 8;
-
-const int EXECUTE_PLANNED_PATH = 9;
-
-
-//responses...
-const int STATUS_UNDEFINED=0;
-const int RECEIVED_AND_INITIATED_RQST=1;
-const int REQUEST_REJECTED_ALREADY_BUSY=2;
-const int SERVER_NOT_BUSY=3;
-const int SERVER_IS_BUSY=4;
-const int RECEIVED_AND_COMPLETED_RQST=5;
-
+//and arm server response codes
+const int ARM_STATUS_UNDEFINED=0;
+const int ARM_RECEIVED_AND_INITIATED_RQST=1;
+const int ARM_REQUEST_REJECTED_ALREADY_BUSY=2;
+const int ARM_SERVER_NOT_BUSY=3;
+const int ARM_SERVER_IS_BUSY=4;
+const int ARM_RECEIVED_AND_COMPLETED_RQST=5;
 
 
 bool g_trigger = false;
-int g_coordinator_mode = DO_NOTHING;
+int g_coordinator_mode = RQST_DO_NOTHING;
 
 tf::TransformListener *g_tfListener_ptr; //pointer to a global transform listener
-   ros::ServiceClient g_pcl_getframe_svc_client; //global service to talk to pcl
-
+ros::ServiceClient g_pcl_getframe_svc_client; //global service to talk to pcl
+ros::ServiceClient g_arm_interface_svc_client;
 
 //use this service to set processing modes interactively
 // this is a very simple interface--expects an integer code in, and nothing gets returned;
@@ -97,7 +101,7 @@ bool coordinatorService(cwru_srv::simple_int_service_messageRequest& request, cw
 
 geometry_msgs::PoseStamped get_model_pose_wrt_torso() {
 geometry_msgs::PoseStamped poseStamped;
-    cwru_srv::IM_node_service_message pcl_getframe_msg;
+    
                     ROS_INFO("requesting model pose from pcl_perception: ");
                     pcl_getframe_msg.request.cmd_mode = 1; // not really used yet...service always assumes you want the model frame, in kinect_pc_frame coords
                     bool status = g_pcl_getframe_svc_client.call(pcl_getframe_msg);
@@ -134,21 +138,54 @@ geometry_msgs::PoseStamped poseStamped;
     
 }
 
+bool arm_server_busy_wait_done() {
+    int status= ARM_SERVER_IS_BUSY;
+    cout<<"waiting on arm server: ";
+    while (status != ARM_SERVER_NOT_BUSY) {
+        arm_nav_msg.request.cmd_mode = ARM_IS_SERVER_BUSY_QUERY;
+        g_arm_interface_svc_client.call(arm_nav_msg); 
+        status = arm_nav_msg.response.rtn_code;
+        cout<<".";
+        //cout<<"status code: "<<status<<endl;
+        ros::spinOnce();
+        ros::Duration(0.1).sleep();
+    }
+    cout<<"done"<<endl;
+    return true;
+}
+
+//utility fnc to convert from a rotation matrix to a geometry_msgs::Quaternion
+geometry_msgs::Quaternion quaternion_from_R(Eigen::Matrix3d R) {
+    geometry_msgs::Quaternion quat_msg;
+    Eigen::Quaterniond  eigen_quat(R);
+    quat_msg.x = eigen_quat.x();
+    quat_msg.y = eigen_quat.y();
+    quat_msg.z = eigen_quat.z();
+    quat_msg.w = eigen_quat.w();   
+    return quat_msg;
+}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "coordinator");
     ros::NodeHandle nh; //standard ros node handle   
     ros::Rate rate(2);
     bool status;
-    cwru_srv::IM_node_service_message IM_6dof_srv_msg;
-    cwru_srv::IM_node_service_message pcl_getframe_msg;
-    cwru_srv::simple_float_service_message reachability_msg;
-    cwru_srv::arm_nav_service_message arm_nav_msg;
 
     geometry_msgs::Pose pose;
     geometry_msgs::PoseStamped poseStamped;
     geometry_msgs::PoseStamped pose_from_pcl, model_pose_wrt_torso;
+    Eigen::Vector3d n_des, t_des, b_des;
+    b_des << 0, 0, -1;
+    n_des << 1, 0, 0;
+    t_des = b_des.cross(n_des);
 
+    Eigen::Matrix3d R_flange_down;
+    R_flange_down.col(0) = n_des;
+    R_flange_down.col(1) = t_des;
+    R_flange_down.col(2) = b_des;
+    
+    geometry_msgs::Quaternion quat_gripper_down = quaternion_from_R(R_flange_down);
+    
     tf::TransformListener tfListener;
     g_tfListener_ptr = &tfListener;
     
@@ -180,13 +217,10 @@ int main(int argc, char **argv) {
     //talk to the reachability node:
     ros::ServiceClient reachability_svc_client = nh.serviceClient<cwru_srv::simple_float_service_message>("compute_reachability_svc");
     // talk to arm interface:
-    ros::ServiceClient arm_interface_svc_client = nh.serviceClient<cwru_srv::arm_nav_service_message>("cartMoveSvc");
-    
-    
-    
+    g_arm_interface_svc_client= nh.serviceClient<cwru_srv::arm_nav_service_message>("cartMoveSvc");
     
     ros::ServiceServer service = nh.advertiseService("coordinator_svc", coordinatorService);
-
+    double des_z_height=0.0;
 
     
     geometry_msgs::Quaternion quaternion;
@@ -195,19 +229,22 @@ int main(int argc, char **argv) {
             g_trigger = false; // reset the trigger
 
             switch (g_coordinator_mode) { // what we do here depends on our mode; mode is settable via a service
-                case DO_NOTHING:
+                case RQST_DO_NOTHING:
                     ROS_INFO("case DO_NOTHING; doing nothing!");
                     break;
-                case MOVE_MARKER_TO_GRASP_POSE:
+                case RQST_MOVE_MARKER_TO_GRASP_POSE:
                     ROS_INFO("case MOVE_MARKER_TO_GRASP_POSE");                    
                     model_pose_wrt_torso = get_model_pose_wrt_torso();
 
-                    // put the marker at the top of the can:
+                    // put the marker origin at the top of the can:
                     pose = model_pose_wrt_torso.pose;
                     pose.position.z += H_CYLINDER;
+                    pose.orientation = quat_gripper_down; // coerce orientation to point down
+                    
                     poseStamped.pose = pose;
                     poseStamped.header.stamp = ros::Time::now();
-                    IM_6dof_srv_msg.request.cmd_mode = SET_NEW_MARKER_POSE;
+                    poseStamped.header.frame_id= "torso";
+                    IM_6dof_srv_msg.request.cmd_mode = IM_SET_NEW_MARKER_POSE;
                     IM_6dof_srv_msg.request.poseStamped_IM_desired = poseStamped;
                     ROS_INFO("placing IM at top of model");
                     status = IM_6dof_svc_client.call(IM_6dof_srv_msg);
@@ -218,58 +255,66 @@ int main(int argc, char **argv) {
                             IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.y,
                             IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.z);*/
                     break;
-                case DISPLAY_REACHABILITY_AT_MARKER_HEIGHT:
-                    IM_6dof_srv_msg.request.cmd_mode = GET_CURRENT_MARKER_POSE;
+                case RQST_DISPLAY_REACHABILITY_AT_MARKER_HEIGHT:
+                    IM_6dof_srv_msg.request.cmd_mode = IM_GET_CURRENT_MARKER_POSE;
                     status = IM_6dof_svc_client.call(IM_6dof_srv_msg);
+                    des_z_height = IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.z;
                     
-                    ROS_INFO("case DISPLAY_REACHABILITY_AT_MARKER_HEIGHT");
+                    ROS_INFO("case DISPLAY_REACHABILITY_AT_MARKER_HEIGHT: z_des = %f",des_z_height);
                     // compute/display reachability at marker height:
-                    reachability_msg.request.request_float32 = pose.position.z;
-                    reachability_svc_client.call(reachability_msg);                    
+                    reachability_msg.request.request_float32 = des_z_height;
+                    reachability_svc_client.call(reachability_msg);  
+                    //arm_server_busy_wait_done(); // no...don't wait on arm server!
                     
                     break;
-                case COMPUTE_MOVE_ARM_TO_PRE_POSE:
+                case RQST_COMPUTE_MOVE_ARM_TO_PRE_POSE:
                     ROS_INFO("case COMPUTE_MOVE_ARM_TO_PRE_POSE");
-                    arm_nav_msg.request.cmd_mode= PLAN_PATH_CURRENT_TO_PRE_POSE;
-                    arm_interface_svc_client.call(arm_nav_msg);
-                    
+                    arm_nav_msg.request.cmd_mode= ARM_PLAN_PATH_CURRENT_TO_PRE_POSE;
+                    g_arm_interface_svc_client.call(arm_nav_msg); //need error checking here
+                    arm_server_busy_wait_done();
+                    arm_nav_msg.request.cmd_mode= ARM_GO_TO_PREDFINED_PRE_POSE;
+                    g_arm_interface_svc_client.call(arm_nav_msg);
                     break;
                 case RQST_DO_PLANNED_PATH:
-                                        ROS_INFO("case RQST_DO_PLANNED_PATH");
-                    arm_nav_msg.request.cmd_mode= EXECUTE_PLANNED_PATH;     
-                    arm_interface_svc_client.call(arm_nav_msg);
-                    
+                    ROS_INFO("case RQST_DO_PLANNED_PATH");
+                    arm_nav_msg.request.cmd_mode= ARM_EXECUTE_PLANNED_PATH;     
+                    g_arm_interface_svc_client.call(arm_nav_msg);
+                    arm_server_busy_wait_done();
                     break;
                 case RQST_DESCEND_20CM:
-                                        ROS_INFO("case RQST_DESCEND_20CM");
-                    arm_nav_msg.request.cmd_mode= DESCEND_20CM;     
-                    arm_interface_svc_client.call(arm_nav_msg);                    
+                    ROS_INFO("case RQST_DESCEND_20CM");
+                    arm_nav_msg.request.cmd_mode= ARM_DESCEND_20CM;     
+                    g_arm_interface_svc_client.call(arm_nav_msg);  
+                    arm_server_busy_wait_done();
                     break;
                  case RQST_ASCEND_20CM:
-                                         ROS_INFO("case RQST_ASCEND_20CM");
-                    arm_nav_msg.request.cmd_mode= DEPART_20CM;     
-                    arm_interface_svc_client.call(arm_nav_msg);                    
+                    ROS_INFO("case RQST_ASCEND_20CM");
+                    arm_nav_msg.request.cmd_mode= ARM_DEPART_20CM;     
+                    g_arm_interface_svc_client.call(arm_nav_msg);   
+                    arm_server_busy_wait_done();
                     break;
                     
-                case MOVE_ARM_TO_APPROACH_POSE:
-                    ROS_INFO("case MOVE_ARM_TO_APPROACH_POSE; doing nothing!");
+                case RQST_MOVE_ARM_TO_APPROACH_POSE:
+                    ROS_INFO("case RQST_MOVE_ARM_TO_APPROACH_POSE; doing nothing!");
                     break;
-                case MOVE_ARM_TO_GRASP_POSE:
-                    ROS_INFO("case MOVE_ARM_TO_GRASP_POSE; doing nothing!");
+                case RQST_MOVE_ARM_TO_GRASP_POSE:
+                    ROS_INFO("case RQST_MOVE_ARM_TO_GRASP_POSE; doing nothing!");
+                    arm_server_busy_wait_done();
                     break;
-                case MOVE_ARM_DEPART:
-                    ROS_INFO("case MOVE_ARM_DEPART; doing nothing!");
+                case RQST_MOVE_ARM_DEPART:
+                    ROS_INFO("case RQST_MOVE_ARM_DEPART; doing nothing!");
+                    arm_server_busy_wait_done();
                     break;
-                case MOVE_ARM_TO_MARKER:
-                    ROS_INFO("I should move arm to marker pose");
-                    IM_6dof_srv_msg.request.cmd_mode = GET_CURRENT_MARKER_POSE;
+                case RQST_MOVE_ARM_TO_MARKER:
+                    ROS_INFO("RQST_MOVE_ARM_TO_MARKER: I should move arm to marker pose");
+                    IM_6dof_srv_msg.request.cmd_mode = IM_GET_CURRENT_MARKER_POSE;
                     status = IM_6dof_svc_client.call(IM_6dof_srv_msg);
                     ROS_INFO("got current marker pose: x,y,z = %f, %f, %f",
                             IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.x,
                             IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.y,
                             IM_6dof_srv_msg.response.poseStamped_IM_current.pose.position.z);
 
-
+                    arm_server_busy_wait_done();       
                     break;
                     /**/
 
